d5916b21456085a22f1cfdd7478d0cede9a8d473
fran <elmendacorp@gmail.com>
2016-11-28 11:34:39 +0100

    representacion interna

--- vDinamico.h
+++ vDinamico.h
@@ -28,16 +28,30 @@ public:
 
     vDinamico(const vDinamico<T> &orig);
 
+    vDinamico(const vDinamico<T> &orig, unsigned inicio, unsigned fin);
+
+    T lee(unsigned pos);
+
+    void escribe(unsigned pos, T &dato);
+
     vDinamico<T> &operator=(const vDinamico<T> &orig);
 
     T &operator[](unsigned pos);
+
+    void inserta(unsigned pos, T &dato);
+
+    T elimina(unsigned pos);
+
+    void aumenta(T &dato);
+
+    T disminuye();
 
     unsigned tama() {
         return (unsigned) tamal;
     }
 
 
-    //T busca(const T &dato);
+    T busca(const T &dato);
 
 
 private:
@@ -82,6 +96,101 @@ vDinamico<T>::vDinamico(const vDinamico<T> &orig) {
     }
 }
 
+/**
+ * constructor de copia por rango
+ * @param orig
+ * @param inicio
+ * @param fin
+ */
+template<typename T>
+vDinamico<T>::vDinamico(const vDinamico<T> &orig, unsigned inicio, unsigned fin) {
+    if (inicio < 0 || inicio > tamal || fin < 0 || fin > tamal || inicio > fin) {
+        throw (std::out_of_range("Fuera de rango"));
+    }
+    if (tamaf < (fin - inicio)) {
+        tamaf = pow(2, ((unsigned) log2((fin - inicio))) + 1);
+        delete[] v;
+        v = new T[tamaf];
+    }
+    tamal = fin - inicio;
+    for (unsigned i = 0; i < (fin - inicio); ++i) {
+        v[i] = orig.v[inicio + i];
+    }
+}
+
+/**
+ * insercion de un elemento en mitad del vector
+ * @param pos
+ * @param dato
+ */
+template<typename T>
+void vDinamico<T>::inserta(unsigned pos, T &dato) {
+    if (pos > tamal || pos < 0) {
+        throw std::out_of_range("Fuera de rango");
+    }
+    if (tamal == tamaf) {
+        T *aux;
+        aux=new T[tamaf *= 2];
+        for (unsigned i = 0; i < tamal; ++i) {
+            aux[i] = v[i];
+        }
+        delete[] v;
+        v = aux;
+    }
+    for (unsigned i = (unsigned)tamal - 1; i >= pos; --i) {
+        v[i+1] = v[i];
+    }
+    v[pos] = dato;
+    ++tamal;
+}
+
+/**
+ * insercion de un elemento al final del vector
+ * @param dato
+ */
+template<typename T>
+void vDinamico<T>::aumenta(T &dato) {
+    if(tamal==0){
+        v=vDinamico<T>();
+    }
+    if (tamal == tamaf) {
+        T *vaux;
+        vaux = new T[tamaf = tamaf * 2];
+        for (unsigned i = 0; i < tamal; i++)
+            vaux[i] = v[i];
+        delete []v;
+        v = vaux;
+    }
+    v[++tamal] = dato;
+
+}
+
+/**
+ * eliminacion del elemento en la posicion iesima
+ * @param pos
+ * @return 
+ */
+template<typename T>
+T vDinamico<T>::elimina(unsigned pos) {
+    if (pos >= tamal || pos < 0) {
+        throw std::out_of_range("Fuera de rango");
+    }
+    T temp = v[pos];
+    for (unsigned i = pos; pos < tamal; ++i) {
+        v[i] = v[i + 1];
+    }
+    if (tamal * 3 < tamaf) {
+        tamaf = tamaf / 2;
+        T *vaux = new T[tamaf];
+        for (unsigned i = 0; i < tamal; i++) {
+            vaux[i] = v[i];
+        };
+        delete []v;
+        v = vaux;
+    }
+    --tamal;
+    return temp;
+}
 
 /**
   * operador cochete
@@ -90,18 +199,8 @@ vDinamico<T>::vDinamico(const vDinamico<T> &orig) {
   */
 template<typename T>
 T &vDinamico<T>::operator[](unsigned pos) {
-    if (pos<0) {
+    if (pos > tamal) {
         throw std::out_of_range("Fuera de rango");
-    }
-    if(pos>=tamaf){
-        tamaf = (int)pow(2, ((unsigned) log2((pos))) + 1);
-        T *vaux = new T[tamaf];
-        for (unsigned i = 0; i < tamal; i++) {
-            vaux[i] = v[i];
-        };
-        delete []v;
-        v = vaux;
-        tamal=pos;
     }
     return v[pos];
 }
@@ -114,6 +213,42 @@ vDinamico<T>::~vDinamico() {
     delete[] v;
 }
 
+/**
+     * Devuelve el objeto en la posicion iesima
+     * @param pos
+     * @return
+     */
+template<typename T>
+T vDinamico<T>::lee(unsigned pos) {
+    if (pos >= tamal||pos<0) {
+        throw std::out_of_range("Fuera de rango");
+    }else {
+        return v[pos];
+    }
+}
+
+/**
+     * Sobreescribir una posicion existente en el vector logico
+     * @param pos
+     * @param dato
+     */
+template<typename T>
+void vDinamico<T>::escribe(unsigned pos, T &dato) {
+    if (pos >= tamal||pos<0) {
+        throw std::out_of_range("Fuera de rango");
+    } else {
+        v[pos] = dato;
+    }
+}
+
+template<typename T>
+T vDinamico<T>::busca(const T &dato) {
+    for (unsigned i = 0; i < tamal; ++i) {
+        if (v[i] == dato) {
+            return v[i];
+        }
+    }
+}
 
 /**
      * COnstructor de copia
@@ -131,6 +266,20 @@ vDinamico<T> &vDinamico<T>::operator=(const vDinamico<T> &orig) {
     return *this;
 }
 
+template<typename T>
+T vDinamico<T>::disminuye() {
+    if (tamal * 3 < tamaf) {
+        tamaf = tamaf / 2;
+        T *vaux = new T[tamaf];
+        for (unsigned i = 0; i < tamal; i++) {
+            vaux[i] = v[i];
+        };
+        delete []v;
+        v = vaux;
+    }
+    tamal--;
+    return v[tamal];
+}
 
 #endif /* VDINAMICO_H */
 
